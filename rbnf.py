ceb15e0980eb39c8fb9e0a685652f462f091dbd9
ajayrfhp <ajayrfhp1710@gmail.com>
2015-07-15 00:29:40 +0530

    done initial Model.Check equation and improve accuracy

--- rbnf.py
+++ rbnf.py
@@ -12,15 +12,33 @@ INTUITION :
 	Weight is not multiplied with inputs ,here act as mean centers .Remember :) 
 	Middle layer weight can be determined by either kmeans or randomly picked datapoints ,one for each neuron in hidden layer
 
+
+
+
 DIMENSION ANALYSIS 
 
 
-	Input 	Weighti 		RBFNeurons 	 	   Weight2  	 Output   
+	Input 	WeightIn		RBFNeurons 	 	   WeightOut	 Output   
 
-  	4*1 	4*1 			4*1                1*4           1*1
+  	4*2 	4*2 			4*1                1*1           4*1
 
+
+REFERENCE LINK
+
+	https://chrisjmccormick.wordpress.com/2013/08/15/radial-basis-function-network-rbfn-tutorial/
 
 '''
+
+def gaussianActivation(vector,beta,mu):
+	
+	vector = vector.reshape(vector.shape[0],1)
+	mu = mu.reshape(mu.shape[0],1)
+	diff = dot(transpose(vector - beta),(vector - beta))
+	value = e**(-beta*diff)
+	return value
+
+
+
 
 
 class NN:
@@ -41,9 +59,45 @@ class NN:
 
 
 	def train(self,inputs,target):
+		self.ai = inputs
 		self.wi = self.ai 
+		
+		# Calculate Mu value
+		
+		average = np.zeros(self.wi[0].shape)
+		for vector in self.wi:
+			average += vector
 
-		# calculate alpha,beta 
+		average /= self.wi.shape[0]
+
+
+
+		# calculate sigma,beta value
+
+		sigma = 0
+		for vector in self.wi:
+			sigma += sum(abs(self.wi - average))/self.wi.shape[0]
+
+		beta = 1 / (2 * sigma * sigma)
+
+
+		
+		for i in range(self.ai.shape[0]):
+			self.ar[i] = gaussianActivation(self.ai[i],beta,average)
+
+
+
+
+		self.wo = dot(linalg.pinv(self.ar),target)	
+
+		self.ao = dot(self.ar,self.wo)
+
+		print self.wi
+		print self.ar
+		print self.wo
+		print self.ao
+			
+		  
 
 		# compute ah values 
 
@@ -54,12 +108,15 @@ class NN:
 
 
 	def predict(self,inputs):
+		return None
 		#PREDICT 
 
 
-data = numpy.array([[0,0],[0,1],[1,0],[1,1]])
-target = [[0],[1],[1],[0],[1],[0]]
+data = np.array([[0,0],[0,1],[1,0],[1,1]])
+target = np.array([[0],[1],[1],[0]])
 
-nn = NN(4,4,1)
+nn = NN(2,4,1)
 nn.train(data,target)
+
+
 
