eefcef3f5822564428a87e291f61ff316e195399
ajayrfhp <ajayrfhp1710@gmail.com>
2015-07-15 14:41:04 +0530

    classified iris dataset with RBNF .Working

--- rbnf.py
+++ rbnf.py
@@ -1,6 +1,8 @@
 ### Neural Network with Radial Basis Funtion 
 import numpy as np 
 from numpy import *
+import csv
+import random
 
 
 
@@ -20,7 +22,10 @@ DIMENSION ANALYSIS
 
 	Input 	WeightIn		RBFNeurons 	 	   WeightOut	 Output   
 
-  	4*2 	4*2 			4*1                1*1           4*1
+
+  	150*4    10*4            150*10              10*3           150*3
+
+  	4*2      4*2             4*4                 4*1            4*1
 
 
 REFERENCE LINK
@@ -33,12 +38,31 @@ def gaussianActivation(vector,beta,mu):
 	
 	vector = vector.reshape(vector.shape[0],1)
 	mu = mu.reshape(mu.shape[0],1)
-	diff = dot(transpose(vector - beta),(vector - beta))
+	diff = dot(transpose(vector - mu),(vector - mu))
 	value = e**(-beta*diff)
+	
 	return value
 
 
+def readFromCSV(filename):
+	data = []
+	with open(filename,"rb") as file:
+		reader = csv.reader(file,delimiter=',')
+		[data.append(row) for row in reader ]
+		for i in range(len(data)):
+			for j in range(len(data[i])):
+				data[i][j] = float(data[i][j])
+	return data
 
+
+def intToVector(data):
+	n = len(set(data))
+	answer = []
+	for i in range(len(data)):
+		tempVector = [0]*n
+		tempVector[int(data[i])] = 1
+		answer.append(tempVector)
+	return answer
 
 
 class NN:
@@ -47,20 +71,27 @@ class NN:
 		self.nr = nr
 		self.no = no
 
-		self.ai = np.ones((self.ni,1))
-		self.ar = np.ones((self.nr,1))
-		self.ao = np.ones((self.no,1))
+		self.sigma = 0
+
+		self.beta = 0
+		self.ai = None
+		self.ar = np.zeros((self.ni,self.nr))
+		self.ao = None
+
 
 
 		self.wi = np.zeros((self.ni,1))
-		self.wo = np.zeros((no,nr))
+		self.wo = np.zeros((nr,no))
+
 
 
 
 
 	def train(self,inputs,target):
 		self.ai = inputs
-		self.wi = self.ai 
+		self.wi = np.array(random.sample(self.ai,self.nr))
+
+
 		
 		# Calculate Mu value
 		
@@ -79,23 +110,27 @@ class NN:
 			sigma += sum(abs(self.wi - average))/self.wi.shape[0]
 
 		beta = 1 / (2 * sigma * sigma)
-
+		self.sigma = sigma
+		self.beta = beta
 
 		
+
 		for i in range(self.ai.shape[0]):
-			self.ar[i] = gaussianActivation(self.ai[i],beta,average)
-
-
+			for j in range(self.wi.shape[0]):
+				self.ar[i][j] = self.ar[i][j] + gaussianActivation(self.ai[i],beta,self.wi[j])
+			
 
 
 		self.wo = dot(linalg.pinv(self.ar),target)	
 
 		self.ao = dot(self.ar,self.wo)
 
-		print self.wi
-		print self.ar
-		print self.wo
-		print self.ao
+		
+
+		#print self.wi
+		#print self.ar
+		#print self.wo
+		#print self.ao
 			
 		  
 
@@ -108,15 +143,60 @@ class NN:
 
 
 	def predict(self,inputs):
-		return None
+		self.ai = inputs
+		self.ar = np.zeros((inputs.shape[0],self.nr))
+
+		for i in range(self.ai.shape[0]):
+			for j in range(self.wi.shape[0]):
+				self.ar[i][j] = self.ar[i][j] + gaussianActivation(self.ai[i],self.beta,self.wi[j])	
+
+		self.ao = dot(self.ar,self.wo)
+		
+
+		
+
+		
+		return self.ao
 		#PREDICT 
 
+	def validate(self,output,expected):
+		cnt = 0
+		i =0
+		for row in self.ao:
+			i+=1
+		print cnt		
 
+
+		
+
+
+
+
+
+
+
+data = readFromCSV("irisData.csv")
+data = np.array(data)
+
+np.random.shuffle(data)
+
+target = data[:,-1]
+target = np.array(intToVector(target))
+data = np.delete(data,-1,1)
+
+
+'''
 data = np.array([[0,0],[0,1],[1,0],[1,1]])
-target = np.array([[0],[1],[1],[0]])
+target = np.array([[1],[2],[1],[0]])
+'''
+target = target.reshape(target.shape[0],target.shape[1])
 
-nn = NN(2,4,1)
-nn.train(data,target)
 
+nn = NN(data.shape[0],10,target.shape[1])
+nn.train(data[0:100],target)
+
+predictions = nn.predict(data[101:])
+
+nn.validate(predictions,target[101:])
 
 
